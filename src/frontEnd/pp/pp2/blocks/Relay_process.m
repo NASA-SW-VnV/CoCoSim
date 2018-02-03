c48483ebe05c8c2d5f90fc8364294f3c2b8b6c0d
Bourbouh <hamza.bourbouh@nasa.gov>
2018-02-03 00:13:59 -0800

    add pre-process of Relay block

---
+++ src/frontEnd/pp/pp2/blocks/Relay_process.m
@@ -0,0 +1,26 @@
+function [] = Relay_process(model)
+% Relay_PROCESS Searches for Relay blocks and replaces them by a
+%  equivalent subsystem.
+%   model is a string containing the name of the model to search in
+
+Relay_list = find_system(model,'BlockType','Relay');
+if not(isempty(Relay_list))
+    display_msg('Processing Relay blocks...', MsgType.INFO, 'Relay_process', ''); 
+    for i=1:length(Relay_list)
+        display_msg(Relay_list{i}, MsgType.INFO, 'Relay_process', ''); 
+        OnSwitchValue = get_param(Relay_list{i},'OnSwitchValue' );
+        OffSwitchValue = get_param(Relay_list{i},'OffSwitchValue' );
+        OnOutputValue = get_param(Relay_list{i},'OnOutputValue' );
+        OffOutputValue = get_param(Relay_list{i},'OffOutputValue' );
+        
+        replace_one_block(Relay_list{i},'pp_lib/relay');
+        set_param(strcat(Relay_list{i},'/OnSwitchValue'),'Value', OnSwitchValue);
+        set_param(strcat(Relay_list{i},'/OffSwitchValue'),'Value', OffSwitchValue);
+        set_param(strcat(Relay_list{i},'/OnOutputValue'),'Value', OnOutputValue);
+        set_param(strcat(Relay_list{i},'/OffOutputValue'),'Value', OffOutputValue);
+        
+    end
+    display_msg('Done\n\n', MsgType.INFO, 'Relay_process', ''); 
+end
+end
+
