e6782dfdd23ecfb3bc49b9248d043cd3f1a22762
Bourbouh <hamza.bourbouh@nasa.gov>
2018-02-04 19:26:21 -0800

    add pre-process of SampleTimeMath block

---
+++ src/frontEnd/pp/pp2/blocks/SampleTimeMath_process.m
@@ -0,0 +1,42 @@
+function [] = SampleTimeMath_process(model)
+% SampleTimeMath_PROCESS Searches for SampleTimeMath blocks and replaces them by a
+%  equivalent subsystem.
+%   model is a string containing the name of the model to search in
+
+SampleTimeMath_list = find_system(model,'BlockType','SampleTimeMath');
+if not(isempty(SampleTimeMath_list))
+    display_msg('Processing SampleTimeMath blocks...', MsgType.INFO, 'SampleTimeMath_process', '');
+    for i=1:length(SampleTimeMath_list)
+        display_msg(SampleTimeMath_list{i}, MsgType.INFO, 'SampleTimeMath_process', '');
+        TsampMathOp = get_param(SampleTimeMath_list{i},'TsampMathOp' );
+        weightValue = get_param(SampleTimeMath_list{i},'weightValue' );
+        try
+            model_sample = SLXUtils.get_BlockDiagram_SampleTime(model);
+            if   model_sample==0 || isnan(model_sample) || model_sample==Inf
+                model_sample = 0.2;
+            end
+        catch
+            model_sample = 0.2;
+        end
+        suffix = TsampMathOp;
+        if strcmp(TsampMathOp, '+')
+            suffix = 'Plus';
+        elseif strcmp(TsampMathOp, '-')
+            suffix = 'Minus';
+        elseif strcmp(TsampMathOp, '*')
+            suffix = 'Multiply';
+        elseif strcmp(TsampMathOp, '/')
+            suffix = 'Divide';
+        elseif strcmp(TsampMathOp, '1/Ts Only')
+            suffix = 'Ts inverse';
+        end
+        pp_block_name = fullfile('pp_lib', strcat('SampleTimeMath', suffix));
+        replace_one_block(SampleTimeMath_list{i},pp_block_name);
+        set_param(strcat(SampleTimeMath_list{i},'/weightValue'),'Value', weightValue);
+        set_param(strcat(SampleTimeMath_list{i},'/Ts'),'Value', num2str(model_sample));
+        
+    end
+    display_msg('Done\n\n', MsgType.INFO, 'SampleTimeMath_process', '');
+end
+end
+
