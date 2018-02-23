dec0c73d8be0a870d5b323b733d2ea5f2a9a007e
ktrinh <ktrinh@messi.ndc.nasa.gov>
2018-02-23 12:48:03 -0800

    improve UnitDelayToLustre

--- src/middleEnd/lustre_multiclocks_compiler/blocks/UnitDelay_To_Lustre.m
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/UnitDelay_To_Lustre.m
@@ -7,7 +7,7 @@ classdef UnitDelay_To_Lustre < Block_To_Lustre
     methods
         
         function  write_code(obj, parent, blk, varargin)
-            [outputs, outputs_dt] = SLX2LusUtils.getBlockOutputsNames(blk)
+            [outputs, outputs_dt] = SLX2LusUtils.getBlockOutputsNames(blk);
             obj.variables = outputs_dt;
             inputs = {};
             
@@ -15,7 +15,7 @@ classdef UnitDelay_To_Lustre < Block_To_Lustre
             nb_inports = numel(widths);
             
             for i=1:nb_inports
-                inputs{i} = SLX2LusUtils.getBlockInputsNames(parent, blk, i)
+                inputs{i} = SLX2LusUtils.getBlockInputsNames(parent, blk, i);
             end
             
             % cast first input if needed to outputDataType
@@ -23,53 +23,48 @@ classdef UnitDelay_To_Lustre < Block_To_Lustre
             % outside. If it is the case, the X0 port number, by
             % convention From Simulink, is the last one.  numel(widths)
             % gives the number of inports therefore the port number of X0.
-
-                if isempty(regexp(blk.InitialCondition, '[a-zA-Z]', 'match'))
-                    InitialCondition = str2num(blk.InitialCondition);
-                    if contains(blk.InitialCondition, '.')
-                        x0DataType = 'double';
-                    else
-                        x0DataType = 'int';
-                    end
-                elseif strcmp(blk.InitialCondition, 'true') ...
-                        ||strcmp(blk.InitialCondition, 'false')
-                    InitialCondition = evalin('base', blk.InitialCondition);
-                    x0DataType = 'boolean';
+            inportDataType = blk.CompiledPortDataTypes.Inport{1};
+            if isempty(regexp(blk.InitialCondition, '[a-zA-Z]', 'match'))
+                x0_condition = str2num(blk.InitialCondition);
+                if contains(blk.InitialCondition, '.')
+                    x0DataType = 'double';
+                elseif strcmp(inportDataType, 'double')
+                    x0_condition = sprintf('%f', x0_condition);
+                    x0DataType = 'double';
                 else
-                    InitialCondition = evalin('base', blk.InitialCondition);
-                    x0DataType =  evalin('base',...
-                        sprintf('class(%s)', blk.InitialCondition));
+                    x0DataType = 'int';
                 end
-                if numel(InitialCondition) > 1
-                    obj.unsupported_options{numel(obj.unsupported_options) + 1} = ...
-                        sprintf('InitialCondition condition %s is not supported in block %s.', ...
-                        num2str(InitialCondition), blk.Origin_path);
-                    return;
-                else
-                    if strncmp(x0DataType, 'int', 3)
-                        InitialCondition = num2str(InitialCondition);
-                    elseif strcmp(x0DataType, 'boolean') || strcmp(x0DataType, 'logical')
-                        if InitialCondition
-                            InitialCondition = 'true';
-                        else
-                            InitialCondition = 'false';
-                        end
+            elseif strcmp(blk.InitialCondition, 'true') ...
+                    ||strcmp(blk.InitialCondition, 'false')
+                x0_condition = evalin('base', blk.InitialCondition);
+                x0DataType = 'boolean';
+            else
+                % the case of parameters from the workspace
+                x0_condition = evalin('base', blk.InitialCondition);
+                x0DataType =  evalin('base',...
+                    sprintf('class(%s)', blk.InitialCondition));
+            end
+            if numel(x0_condition) > 1
+                obj.unsupported_options{numel(obj.unsupported_options) + 1} = ...
+                    sprintf('InitialCondition condition %s is not supported in block %s.', ...
+                    num2str(x0_condition), blk.Origin_path);
+                return;
+            else
+                if strncmp(x0DataType, 'int', 3) ...
+                        || strncmp(x0DataType, 'uint', 4)
+                    x0_condition = num2str(x0_condition);
+                elseif strcmp(x0DataType, 'boolean') || strcmp(x0DataType, 'logical')
+                    if x0_condition
+                        x0_condition = 'true';
                     else
-                        InitialCondition = sprintf('%.15f', InitialCondition);
+                        x0_condition = 'false';
                     end
-                end
-                inputs{end+1} = {InitialCondition};
-                
-                I = [1, (nb_inports+1)];
-                widths(end+1) = 1;
- 
-            max_width = max(widths(I));
-            for i=I
-                if numel(inputs{i}) < max_width
-                    inputs{i} = arrayfun(@(x) {inputs{i}{1}}, (1:max_width));
+                else
+                    x0_condition = sprintf('%.15f', x0_condition);
                 end
             end
-            inportDataType = blk.CompiledPortDataTypes.Inport{1};
+            
+            
             
             %converts the x0 data type(s) to the first inport data type
             %(Simulink documentation)
@@ -78,16 +73,15 @@ classdef UnitDelay_To_Lustre < Block_To_Lustre
                 if ~isempty(external_lib)
                     obj.external_libraries = [obj.external_libraries,...
                         external_lib];
-                    inputs{end} = cellfun(@(x) sprintf(conv_format,x), inputs{end}, 'un', 0);
+                    x0_condition = sprintf(conv_format, x0_condition);
                 end
             end
-
-            x0 =  inputs{end};
+            
             u = inputs{1};
             
             codes = {};
             for j=1:numel(u)
-                code =  Delay_To_Lustre.getExpofNDelays(x0{j}, u{j}, 1);
+                code =  sprintf(' %s -> pre(%s) ', x0_condition , u{j});
                 codes{j} = sprintf('%s = %s;\n\t', outputs{j}, code);
             end
             
@@ -102,17 +96,7 @@ classdef UnitDelay_To_Lustre < Block_To_Lustre
         end
     end
     
-    methods(Static = true)
-        function code = getExpofNDelays(x0, u, n)
-            
-            if n == 0
-                code = sprintf(' %s ' , u);
-            else
-                code = sprintf(' %s -> pre(%s) ', x0 , Delay_To_Lustre.getExpofNDelays(x0, u, n -1));
-            end
-            
-        end
-    end
+    
     
 end
 
