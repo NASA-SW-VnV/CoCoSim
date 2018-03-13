2291926051e76af458139d52047563c1503dac61
ktrinh <khanh.v.trinh@nasa.gov>
2018-03-13 12:36:28 -0700

    Added vector code to Concatenate_To_Lustre.m

--- src/middleEnd/lustre_multiclocks_compiler/blocks/Concatenate_To_Lustre.m
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/Concatenate_To_Lustre.m
@@ -36,41 +36,51 @@ classdef Concatenate_To_Lustre < Block_To_Lustre
                     end
                 end
             end
-           
-            in_matrix_dimension = Product_To_Lustre.getInputMatrixDimensions(blk);
-            
-            codes = {};
-            if strcmp(blk.ConcatenateDimension, '1')
-                index = 0;
-                for i=1:numel(in_matrix_dimension)       %loop over number of inports
-                    origColLen = in_matrix_dimension{i}.dims(1);
-                    for j=1:in_matrix_dimension{i}.dims(1);     % loop over each inport array 1st dimension
-                        for k=1:in_matrix_dimension{i}.dims(2)                            % loop over each inport array 2nd  dimension, vector is also treated as 2Ds
-                            index = index + 1;
-                            inputIndex = origColLen*(j-1)+k
-                            codes{index} = sprintf('%s = %s;\n\t', outputs{index}, inputs{i}{inputIndex})
-                        end
+                       
+            isVector = strcmp(blk.Mode,'Vector');
+            codes = {}; 
+            if isVector
+                outputIndex = 0;
+                for i=1:numel(widths)
+                    for j=1:numel(inputs{i})
+                        outputIndex = outputIndex + 1;
+                        codes{outputIndex} = sprintf('%s = %s;\n\t', outputs{outputIndex}, inputs{i}{j});
                     end
                 end
             else
-                outputIndex = 0;
-                origRowLen = round(in_matrix_dimension{1}.dims(1));
-                origColLen = round(in_matrix_dimension{1}.dims(2));
-                matrixSize = round(origRowLen*origRowLen);
-                numOutputRows = origRowLen;
-                numOutputColumns = numel(in_matrix_dimension)*origColLen;
-                for i=1:numOutputRows       %loop over number of inports
-                    for j=1:numOutputColumns    % loop over each inport array 1st dimension
-                        outputIndex = outputIndex + 1;                        
-                        inputPortIndex = floor((j-1)/origColLen)+ 1;
-                        rowIndex = i;
-                        columnIndex = rem(j,origColLen);  
-                        if columnIndex==0
-                            columnIndex = origColLen;
+                in_matrix_dimension = Product_To_Lustre.getInputMatrixDimensions(blk);
+                if strcmp(blk.ConcatenateDimension, '1')
+                    index = 0;
+                    for i=1:numel(in_matrix_dimension)       %loop over number of inports
+                        origColLen = in_matrix_dimension{i}.dims(1);
+                        for j=1:in_matrix_dimension{i}.dims(1);     % loop over each inport array 1st dimension
+                            for k=1:in_matrix_dimension{i}.dims(2)                            % loop over each inport array 2nd  dimension, vector is also treated as 2Ds
+                                index = index + 1;
+                                inputIndex = origColLen*(j-1)+k
+                                codes{index} = sprintf('%s = %s;\n\t', outputs{index}, inputs{i}{inputIndex})
+                            end
                         end
-                        a = sprintf('out %d, in mat %d, row %d, col %d',outputIndex, inputPortIndex, rowIndex, columnIndex)
-                        inputIndex = (rowIndex-1)*origColLen + columnIndex;
-                        codes{outputIndex} = sprintf('%s = %s;\n\t', outputs{outputIndex}, inputs{inputPortIndex}{inputIndex});
+                    end
+                else
+                    outputIndex = 0;
+                    origRowLen = round(in_matrix_dimension{1}.dims(1));
+                    origColLen = round(in_matrix_dimension{1}.dims(2));
+                    matrixSize = round(origRowLen*origRowLen);
+                    numOutputRows = origRowLen;
+                    numOutputColumns = numel(in_matrix_dimension)*origColLen;
+                    for i=1:numOutputRows       %loop over number of inports
+                        for j=1:numOutputColumns    % loop over each inport array 1st dimension
+                            outputIndex = outputIndex + 1;
+                            inputPortIndex = floor((j-1)/origColLen)+ 1;
+                            rowIndex = i;
+                            columnIndex = rem(j,origColLen);
+                            if columnIndex==0
+                                columnIndex = origColLen;
+                            end
+                            a = sprintf('out %d, in mat %d, row %d, col %d',outputIndex, inputPortIndex, rowIndex, columnIndex)
+                            inputIndex = (rowIndex-1)*origColLen + columnIndex;
+                            codes{outputIndex} = sprintf('%s = %s;\n\t', outputs{outputIndex}, inputs{inputPortIndex}{inputIndex});
+                        end
                     end
                 end
             end
