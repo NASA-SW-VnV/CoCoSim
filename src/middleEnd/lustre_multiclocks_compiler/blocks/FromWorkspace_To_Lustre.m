06a5f7920323430795d54b9b0a08ef4b94ef4b18
ktrinh <khanh.v.trinh@nasa.gov>
2018-03-21 14:49:06 -0700

    Added struct to FromWorkspace_To_Lustre

--- src/middleEnd/lustre_multiclocks_compiler/blocks/FromWorkspace_To_Lustre.m
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/FromWorkspace_To_Lustre.m
@@ -23,33 +23,49 @@ classdef FromWorkspace_To_Lustre < Block_To_Lustre
             outputDataType = blk.CompiledPortDataTypes.Outport{1};
             
             % there is no inputs
-%             for i=1:nbInputs
-%                 inputs{i} = SLX2LusUtils.getBlockInputsNames(parent, blk, i);
-%                 if numel(inputs{i}) < max_width
-%                     inputs{i} = arrayfun(@(x) {inputs{i}{1}}, (1:max_width));
-%                 end
-%                 inport_dt = blk.CompiledPortDataTypes.Inport(i);
-%                 %converts the input data type(s) to
-%                 %its accumulator data type
-%                 if ~strcmp(inport_dt, outputDataType)
-%                     [external_lib, conv_format] = SLX2LusUtils.dataType_conversion(inport_dt, outputDataType);
-%                     if ~isempty(external_lib)
-%                         obj.addExternal_libraries(external_lib);
-%                         inputs{i} = cellfun(@(x) sprintf(conv_format,x), inputs{i}, 'un', 0);
-%                     end
-%                 end
-%             end
-%             VariableName = blk.VariableName
-%             SampleTime  = blk.SampleTime
-%             Interpolate = blk.Interpolate
-%             ZeroCross = blk.ZeroCross
-%             OutputAfterFinalValue = blk.OutputAfterFinalValue
             
             VariableName = blk.VariableName;
             variable = evalin('base',VariableName);
             [outLusDT, ~, one] = SLX2LusUtils.get_lustre_dt(outputDataType);
-            codes = {};
-
+            
+            
+            % blk parameters
+%             SampleTime = blk.SampleTime;
+%             Interpolate = blk.Interpolate;
+%             ZeroCross = blk.ZeroCross;
+%             OutputAfterFinalValue = blk.OutputAfterFinalValue;
+            
+            codes = {};          
+            
+            if isnumeric(variable)
+                % for matrix
+                [nrow, ncol] = size(variable);
+                t = variable(:,1);
+                values = variable(:,2:ncol);
+                dims = ncol - 1;
+            elseif isstruct(variable)
+                % for struct
+                t = variable.time;
+                nrow = numel(t);
+                values = variable.signals.values;
+                dims = variable.signals.dimensions;
+            else                
+                display_msg(sprintf('Workspace variable must be numeric arrays or struct in block %s',...
+                    blk.Origin_path), MsgType.ERROR, 'FromWorkspace_To_Lustre', '');            
+            end
+            
+            code = '';
+            for i=1:dims
+                for j=nrow:-1:1
+                    a = values(j,i);
+                    if j== nrow
+                        code = sprintf('%f',a);
+                    else
+                        code = sprintf('%f -> (%s)',a,code);
+                    end
+                end
+                codes{i} = sprintf('%s = %s; \n\t',outputs{i},code);
+            end
             
             obj.setCode(MatlabUtils.strjoin(codes, ''));
             obj.addVariable(outputs_dt);
@@ -57,8 +73,13 @@ classdef FromWorkspace_To_Lustre < Block_To_Lustre
         
         function options = getUnsupportedOptions(obj, blk, varargin)
             obj.unsupported_options = {};
-
-           
+            VariableName = blk.VariableName;
+            variable = evalin('base',VariableName);
+            if ~isnumeric(variable) || ~isstruct(variable)
+                obj.addUnsupported_options(...
+                    sprintf('Workspace variable must be numeric arrays or struct in block %s',...
+                    blk.Origin_path), MsgType.ERROR, 'FromWorkspace_To_Lustre', '');
+            end
             options = obj.unsupported_options;
         end
     end
