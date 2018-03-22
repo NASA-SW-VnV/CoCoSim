2e6b5d9316d5eeca8f626b448637a1076705d6ab
ktrinh <khanh.v.trinh@nasa.gov>
2018-03-21 17:34:10 -0700

    Added Mux_To_Lustre

---
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/Mux_To_Lustre.m
@@ -0,0 +1,61 @@
+classdef Mux_To_Lustre < Block_To_Lustre
+    % Mux_To_Lustre
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Copyright (c) 2017 United States Government as represented by the
+    % Administrator of the National Aeronautics and Space Administration.
+    % All Rights Reserved.
+    % Author: Trinh, Khanh V <khanh.v.trinh@nasa.gov>
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    properties
+    end
+    
+    methods
+        
+        function  write_code(obj, parent, blk, varargin)
+            [outputs, outputs_dt] = SLX2LusUtils.getBlockOutputsNames(blk);
+            inputs = {};
+            
+            widths = blk.CompiledPortWidths.Inport;
+            max_width = max(widths);
+            outputDataType = blk.CompiledPortDataTypes.Outport{1};
+
+            for i=1:numel(widths)
+                inputs{i} = SLX2LusUtils.getBlockInputsNames(parent, blk, i);
+                inport_dt = blk.CompiledPortDataTypes.Inport(i);
+                %converts the input data type(s) to
+                %its accumulator data type
+                if ~strcmp(inport_dt, outputDataType)
+                    [external_lib, conv_format] = SLX2LusUtils.dataType_conversion(inport_dt, outputDataType);
+                    if ~isempty(external_lib)
+                        obj.addExternal_libraries(external_lib);
+                        inputs{i} = cellfun(@(x) sprintf(conv_format,x), inputs{i}, 'un', 0);
+                    end
+                end
+            end
+                       
+            codes = {};
+            if 1
+                outputIndex = 0;
+                for i=1:numel(widths)
+                    for j=1:numel(inputs{i})
+                        outputIndex = outputIndex + 1;
+                        codes{outputIndex} = sprintf('%s = %s;\n\t', outputs{outputIndex}, inputs{i}{j});
+                    end
+                end
+
+            end
+            
+            obj.setCode(MatlabUtils.strjoin(codes, ''));
+            obj.addVariable(outputs_dt);
+        end
+        
+        function options = getUnsupportedOptions(obj, blk, varargin)
+            obj.unsupported_options = {};
+           
+            options = obj.unsupported_options;
+        end
+    end
+    
+end
+
