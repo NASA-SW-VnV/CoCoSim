8ad5340b3b1b5c93ff1070e697a6eda34f55f53b
ktrinh <khanh.v.trinh@nasa.gov>
2018-03-23 19:47:55 -0700

    Added index options to Assignment_To_Lustre:

--- src/middleEnd/lustre_multiclocks_compiler/blocks/Assignment_To_Lustre.m
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/Assignment_To_Lustre.m
@@ -35,76 +35,113 @@ classdef Assignment_To_Lustre < Block_To_Lustre
             end
             
             %             OutputInitialize = blk.OutputInitialize;
-            %             IndexMode = blk.IndexMode;
-            %             [NumberOfDimensions, ~, ~] = ...
-            %                 Constant_To_Lustre.getValueFromParameter(parent, blk, blk.NumberOfDimensions);
+            IndexMode = blk.IndexMode;
+            [NumberOfDimensions, ~, ~] = ...
+                Constant_To_Lustre.getValueFromParameter(parent, blk, blk.NumberOfDimensions);
             %             IndexOptions = blk.IndexOptions;
             %             [Indices, ~, ~] = ...
             %                 Constant_To_Lustre.getValueFromParameter(parent, blk, blk.Indices);
             %             OutputSizes = blk.OutputSizes;
             codes = {};
-            if isempty(find(~strcmp(blk.IndexOptionArray, 'Index vector (dialog)'), 1))
-                for i=1:numel(blk.IndexParamArray)
+            %if isempty(find(~strcmp(blk.IndexOptionArray, 'Index vector (dialog)'), 1))
+            
+            
+            in_matrix_dimension = Reshape_To_Lustre.getInputMatrixDimensions(blk);
+            if in_matrix_dimension{1}.numDs == 1
+                numColY0 = in_matrix_dimension{1}.dims(1);
+                onDimension = true;
+            else
+                onDimension = false;
+                numColY0 = in_matrix_dimension{1}.dims(2);
+            end
+            if in_matrix_dimension{2}.numDs == 1
+                numRowU = 1;
+                numColU = in_matrix_dimension{2}.dims(1);
+            else
+                numRowU = in_matrix_dimension{2}.dims(1);
+                numColU = in_matrix_dimension{2}.dims(2);
+            end
+            
+            indexPortNumber = 0;
+            for i=1:numel(blk.IndexOptionArray)
+                if strcmp(blk.IndexOptionArray{i}, 'Assign all')
+                    if i==1
+                        ind{i} = (1:numRowU);
+                    else
+                        ind{i} = (1:numColU);
+                    end
+                elseif strcmp(blk.IndexOptionArray{i}, 'Index vector (dialog)')
                     [Idx, ~, ~] = ...
                         Constant_To_Lustre.getValueFromParameter(parent, blk, blk.IndexParamArray{i});
                     ind{i} = Idx;
-                end
-                
-                in_matrix_dimension = Reshape_To_Lustre.getInputMatrixDimensions(blk);
-                if in_matrix_dimension{1}.numDs == 1
-                    numColY0 = in_matrix_dimension{1}.dims(1);
-                    onDimension = true;
+                elseif strcmp(blk.IndexOptionArray{i}, 'Index vector (port)')
+                    indexPortNumber = indexPortNumber + 1;
+                    portNumber = indexPortNumber + 2;   % 1st and 2nd for Y0 and U
+                    indexBlock = SLX2LusUtils.getpreBlock(parent, blk, portNumber);
+                    [ind{i}, ~, status] = ...
+                        Constant_To_Lustre.getValueFromParameter(parent, indexBlock, indexBlock.Value);
+                elseif strcmp(blk.IndexOptionArray{i}, 'Starting index (dialog)')
+                    [Idx, ~, ~] = ...
+                        Constant_To_Lustre.getValueFromParameter(parent, blk, blk.IndexParamArray{i});
+                    if i==1
+                        ind{i} = (Idx:Idx+numRowU-1);
+                    else
+                        ind{i} = (Idx:Idx+numColU-1);
+                    end
+                    
+                elseif strcmp(blk.IndexOptionArray{i}, 'Starting index (port)')
+                    indexPortNumber = indexPortNumber + 1;
+                    portNumber = indexPortNumber + 2;   % 1st and 2nd for Y0 and U
+                    indexBlock = SLX2LusUtils.getpreBlock(parent, blk, portNumber);
+                    [startIndex, ~, status] = ...
+                        Constant_To_Lustre.getValueFromParameter(parent, indexBlock, indexBlock.Value);
+                    if i==1
+                        ind{i} = (startIndex:startIndex+numRowU-1);
+                    else
+                        ind{i} = (startIndex:startIndex+numColU-1);
+                    end
                 else
-                    onDimension = false;
-                    numColY0 = in_matrix_dimension{1}.dims(2);
+                    % should not be here
+                    display_msg(sprintf('IndexOption  %s not recognized in block %s',...
+                        blk.IndexOptionArray{i}, indexBlock.Origin_path), ...
+                        MsgType.ERROR, 'Assignment_To_Lustre', '');
                 end
-                if in_matrix_dimension{2}.numDs == 1
-                    numRowU = 1;
-                    numColU = in_matrix_dimension{2}.dims(1);
-                else
-                    numRowU = in_matrix_dimension{2}.dims(1);
-                    numColU = in_matrix_dimension{2}.dims(2);
+            end
+                                
+            % hard code for 2D.  do recursive call for n dims later
+            indexToBeAssigned = [];
+            codeIndex = 0;
+            if onDimension
+                for colU=1:numColU
+                    out_j = ind{1}(colU);
+                    outIndex =  out_j;
+                    Uindex =  colU;
+                    indexToBeAssigned = [indexToBeAssigned, outIndex];
+                    codeIndex = codeIndex + 1;
+                    codes{codeIndex} = sprintf('%s = %s ;\n\t', outputs{outIndex}, inputs{2}{Uindex});
                 end
-                
-                
-                
-                % hard code for 2D.  do recursive call for n dims later
-                indexToBeAssigned = [];
-                codeIndex = 0;
-                if onDimension
+            else
+                for rowU=1:numRowU
                     for colU=1:numColU
-                         out_j = ind{1}(colU);
-                         outIndex =  out_j;
-                         Uindex =  colU;
-                         indexToBeAssigned = [indexToBeAssigned, outIndex];
-                         codeIndex = codeIndex + 1;
-                         codes{codeIndex} = sprintf('%s = %s ;\n\t', outputs{outIndex}, inputs{2}{Uindex});
-                    end
-                else
-                    for rowU=1:numRowU
-                        for colU=1:numColU
-                            out_i = ind{1}(rowU);
-                            out_j = ind{2}(colU);
-                            outIndex = (out_i-1)*numColY0 + out_j;
-                            Uindex = (rowU-1)*numColU + colU;
-                            indexToBeAssigned = [indexToBeAssigned, outIndex];
-                            codeIndex = codeIndex + 1;
-                            codes{codeIndex} = sprintf('%s = %s ;\n\t', outputs{outIndex}, inputs{2}{Uindex});
-                        end
-                    end
-                end
-                
-                % write unassigned element
-                for i=1:numel(outputs)
-                    if ~ismember(i,indexToBeAssigned)
+                        out_i = ind{1}(rowU);
+                        out_j = ind{2}(colU);
+                        outIndex = (out_i-1)*numColY0 + out_j;
+                        Uindex = (rowU-1)*numColU + colU;
+                        indexToBeAssigned = [indexToBeAssigned, outIndex];
                         codeIndex = codeIndex + 1;
-                        codes{codeIndex} = sprintf('%s = %s ;\n\t', outputs{i}, inputs{1}{i});
+                        codes{codeIndex} = sprintf('%s = %s ;\n\t', outputs{outIndex}, inputs{2}{Uindex});
                     end
                 end
             end
             
-            
-            
+            % write unassigned element
+            for i=1:numel(outputs)
+                if ~ismember(i,indexToBeAssigned)
+                    codeIndex = codeIndex + 1;
+                    codes{codeIndex} = sprintf('%s = %s ;\n\t', outputs{i}, inputs{1}{i});
+                end
+            end
+                        
             obj.setCode(MatlabUtils.strjoin(codes, ''));
             obj.addVariable(outputs_dt);
         end
