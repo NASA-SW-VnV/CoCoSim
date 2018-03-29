c101aa76e85f752a35ed86d7d0fa6e85e46cd287
ktrinh <khanh.v.trinh@nasa.gov>
2018-03-28 17:16:53 -0700

    Fix Reshape_To_Lustre to column inline convention

--- src/middleEnd/lustre_multiclocks_compiler/blocks/Reshape_To_Lustre.m
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/Reshape_To_Lustre.m
@@ -17,14 +17,10 @@ classdef Reshape_To_Lustre < Block_To_Lustre
             inputs = {};
             
             widths = blk.CompiledPortWidths.Inport;
-            max_width = max(widths);
             outputDataType = blk.CompiledPortDataTypes.Outport{1};
 
             for i=1:numel(widths)
                 inputs{i} = SLX2LusUtils.getBlockInputsNames(parent, blk, i);
-                if numel(inputs{i}) < max_width
-                    inputs{i} = arrayfun(@(x) {inputs{i}{1}}, (1:max_width));
-                end
                 inport_dt = blk.CompiledPortDataTypes.Inport(i);
                 %converts the input data type(s) to
                 %its accumulator data type
@@ -36,60 +32,11 @@ classdef Reshape_To_Lustre < Block_To_Lustre
                     end
                 end
             end
-                       
-            outputDimensionality = blk.OutputDimensionality;   % 1D array, row vector, column vector, customize, deriveInputPort
-            dimensionType = 1;    % 1D array, row vector, and column vector
-            outputDimensions = blk.OutputDimensions;
-            if strcmp(blk.OutputDimensionality, 'Customize')
-                dimensionType = 2;    % 2 for Customize and use outputDimensions
-            elseif strcmp(blk.OutputDimensionality, 'Derive from reference input port')
-                dimensionType = 3;
-            end
             
-            in_matrix_dimension = Reshape_To_Lustre.getInputMatrixDimensions(blk);
-            codes = {}; 
-            if dimensionType == 1 || dimensionType == 2 || dimensionType == 3
-
-                % in matrix = nxm (i_in,j_in)        out matrix = kxl (i_out, j_out)
-                n = in_matrix_dimension{1}.dims(1,1);                
-                if in_matrix_dimension{1}.numDs==1
-                    m = 1;
-                elseif in_matrix_dimension{1}.numDs==2
-                    m = in_matrix_dimension{1}.dims(1,2)
-                else
-                    display_msg(sprintf('3 or more dimensions matrix is not supported in block %s',...
-                        blk.Origin_path), MsgType.WARNING, 'Reshape_To_Lustre', '');
-                end
-                if dimensionType == 1
-                    k = numel(outputs);
-                    l = 1;
-                else
-                    outmat_size = str2num(blk.OutputDimensions)
-                    k = outmat_size(1);
-                    l = outmat_size(2);
-                end
-                for i=1:numel(outputs)
-                    i_in = rem(i,n);
-                    j_in = fix(i/n)+1;
-                    if rem(i,n) == 0
-                        i_in = n;
-                        j_in = j_in - 1;
-                    end
-                    inIndex = (i_in-1)*m + j_in;
-                    i_out = rem(i,k);
-                    j_out = fix(i/k)+1;
-                    if rem(i,k) == 0
-                        i_out = k;
-                        j_out = j_out - 1;
-                    end
-                    outIndex = (i_out-1)*l + j_out;
-                    sprintf('i %d, inIndex %d, outIndex %d, i_in %d, j_in %d, i_out %d, j_out %d \n\t',i, inIndex, outIndex, i_in, j_in, i_out, j_out)
-                    codes{i} = sprintf('%s = %s;\n\t', outputs{outIndex}, inputs{1}{inIndex});
-                end
-                
-            else
-                display_msg(sprintf('Unknown Output dimensionality in block %s',...
-                    blk.Origin_path), MsgType.WARNING, 'Reshape_To_Lustre', '');
+            % reshape doesn't do anything. Just pass inputs to outputs.
+            % PP?
+            for i=1:numel(outputs)
+                codes{i} = sprintf('%s = %s;\n\t', outputs{i}, inputs{1}{i});
             end
             
             obj.setCode(MatlabUtils.strjoin(codes, ''));
@@ -98,44 +45,17 @@ classdef Reshape_To_Lustre < Block_To_Lustre
         
         function options = getUnsupportedOptions(obj, blk, varargin)
             obj.unsupported_options = {};
-           
+            if strcmp(blk.BusSelectionMode, 'on')
+                obj.addUnsupported_options(...
+                    sprintf('Derive from reference input port is not supported in block %s',...
+                    blk.Origin_path), ...
+                    MsgType.ERROR, 'Reshape_To_Lustre', '');
+            end            
             options = obj.unsupported_options;
         end
     end
     
-    
-    methods(Static)
-        % This method allows for only 1 input, the same static under
-        % Product_To_Lustre may require more than 1 input
-        function in_matrix_dimension = getInputMatrixDimensions(blk)
-            % return structure of matrix size
-            in_matrix_dimension = {};
-            readMatrixDimension = true;
-            numMat = 0;
-            inport_dimensions = blk.CompiledPortDimensions.Inport;
-            
-            for i=1:numel(inport_dimensions)
-                if readMatrixDimension
-                    numMat = numMat + 1;
-                    numDs = inport_dimensions(i);
-                    readMatrixDimension = false;
-                    in_matrix_dimension{numMat}.numDs = numDs;
-                    in_matrix_dimension{numMat}.dims = zeros(1,numDs);
-                    index = 0;
-                else
-                    index = index + 1;                    
-                    in_matrix_dimension{numMat}.dims(1,index) = inport_dimensions(i);
-                    if index == numDs;
-                        readMatrixDimension = true;
-                    end
-                end
-                
-            end
-        end
-       
-    end    
-    
-    
+
     
 end
 
