b09020429d045547570cb123aab7cd27fb1453ca
ktrinh <khanh.v.trinh@nasa.gov>
2018-04-06 18:25:11 -0700

    Added Rounding_To_Lustre.m DataTypeConversion.m

---
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/Rounding_To_Lustre.m
@@ -0,0 +1,54 @@
+classdef Rounding_To_Lustre < Block_To_Lustre
+    % RoundingFunction_To_Lustre
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Copyright (c) 2017 United States Government as represented by the
+    % Administrator of the National Aeronautics and Space Administration.
+    % All Rights Reserved.
+    % Author: Trinh, Khanh V <khanh.v.trinh@nasa.gov>
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    properties
+    end
+    
+    methods
+        
+        function  write_code(obj, parent, blk, varargin)
+            [outputs, outputs_dt] = SLX2LusUtils.getBlockOutputsNames(blk);
+            inputs = {};
+            
+            widths = blk.CompiledPortWidths.Inport;
+            outputDataType = blk.CompiledPortDataTypes.Outport{1};
+
+            for i=1:numel(widths)
+                inputs{i} = SLX2LusUtils.getBlockInputsNames(parent, blk, i);
+                inport_dt = blk.CompiledPortDataTypes.Inport(i);
+                %converts the input data type(s) to
+                %its outputDataType
+                if ~strcmp(inport_dt, outputDataType)
+                    [external_lib, conv_format] = SLX2LusUtils.dataType_conversion(inport_dt, outputDataType);
+                    if ~isempty(external_lib)
+                        obj.addExternal_libraries(external_lib);
+                        inputs{i} = cellfun(@(x) sprintf(conv_format,x), inputs{i}, 'un', 0);
+                    end
+                end
+            end
+            
+            % Just pass inputs to outputs.
+            for i=1:numel(outputs)
+                codes{i} = sprintf('%s = %s;\n\t', outputs{i}, inputs{1}{i});
+            end
+            
+            obj.setCode(MatlabUtils.strjoin(codes, ''));
+            obj.addVariable(outputs_dt);
+        end
+        
+        function options = getUnsupportedOptions(obj, blk, varargin)
+            obj.unsupported_options = {};
+            options = obj.unsupported_options;
+        end
+    end
+    
+
+    
+end
+
