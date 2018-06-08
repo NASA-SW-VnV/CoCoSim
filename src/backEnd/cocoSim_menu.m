141d004495dbacc212f9a3ce4afda458da2bafe7
Bourbouh <hamza.bourbouh@nasa.gov>
2018-06-07 22:53:47 -0700

    clean up

--- src/backEnd/cocoSim_menu.m
+++ src/backEnd/cocoSim_menu.m
@@ -12,7 +12,17 @@ schema.label = 'CoCoSim';
 schema.statustip = 'Automated Analysis Framework';
 schema.autoDisableWhen = 'Busy';
 
-cocoSim_menu_config;
+[backEnd_root, ~, ~] = fileparts(mfilename('fullpath'));
+menue_items = {};
+menue_items{end + 1} = fullfile(backEnd_root, 'unsupported_blocks','unsupportedBlocksMenu.m');
+menue_items{end + 1} = fullfile(backEnd_root, 'verification','verificationMenu.m');
+menue_items{end + 1} = fullfile(backEnd_root, 'test_case_generation','TestCaseGenMenu.m');
+menue_items{end + 1} = fullfile(backEnd_root, 'generate_invariants','generateInvariantsMenu.m');
+menue_items{end + 1} = fullfile(backEnd_root, 'importLustreRequirements','importLusReqMenu.m');
+menue_items{end + 1} = fullfile(backEnd_root, 'generate_code','generateCodeMenu.m');
+menue_items{end + 1} = fullfile(backEnd_root, 'validation','validationMenu.m');
+menue_items{end + 1} = fullfile(backEnd_root, 'extra_options','extraOptionsMenu.m');
+
 schema.childrenFcns = cellfun(@MenuUtils.funPath2Handle, menue_items,...
                     'UniformOutput', false);
 
