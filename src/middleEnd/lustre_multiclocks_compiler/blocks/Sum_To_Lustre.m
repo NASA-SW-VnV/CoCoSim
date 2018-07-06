8f3b5c17c843dbd73786a380254b439318cb03be
ktrinh <khanh.v.trinh@nasa.gov>
2018-07-05 18:45:44 -0700

    fix bugs in Sum and Product.  Added variation graph for Sum and Product

--- src/middleEnd/lustre_multiclocks_compiler/blocks/Sum_To_Lustre.m
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/Sum_To_Lustre.m
@@ -107,7 +107,8 @@ classdef Sum_To_Lustre < Block_To_Lustre
                         MsgType.ERROR, 'Sum_To_Lustre', '');
                     return;
                 end
-                [codes] = Sum_To_Lustre.oneInputSumProduct(blk, outputs, inputs, CollapseDim, widths, exp, initCode);
+                [codes] = Sum_To_Lustre.oneInputSumProduct(blk, outputs, ...
+                    inputs, CollapseDim, widths, exp, initCode,isSumBlock, conv_format);
             else
                 if ~isSumBlock && strcmp(blk.Multiplication, 'Matrix(*)')
                     %This is a matrix multiplication, only applies to
@@ -193,7 +194,8 @@ classdef Sum_To_Lustre < Block_To_Lustre
             end
         end
         %%
-        function [codes] = oneInputSumProduct(blk, outputs, inputs, CollapseDim, widths, exp, initCode)
+        function [codes] = oneInputSumProduct(blk, outputs, inputs, CollapseDim, ...
+                widths, exp, initCode,isSumBlock, conv_format)
             if ~isSumBlock && strcmp(blk.Multiplication, 'Matrix(*)')    % product, 1 input, 1 exp, Matrix(x), matrix remains unchanged.
                 for i=1:numel(outputs)
                     codes{i} = sprintf('%s = %s;\n\t', outputs{i}, inputs{1}{i});
@@ -215,7 +217,7 @@ classdef Sum_To_Lustre < Block_To_Lustre
             elseif numel(outputs)>1        % needed for collapsing of matrix
                 in_matrix_dimension = Assignment_To_Lustre.getInputMatrixDimensions(blk.CompiledPortDimensions.Inport);
                 [numelCollapseDim, delta, collapseDims] = Sum_To_Lustre.collapseMatrix(in_matrix_dimension, CollapseDim);
-                
+                matSize = in_matrix_dimension{1}.dims;
                 for i=1:numel(outputs)
                     code = initCode;
                     
