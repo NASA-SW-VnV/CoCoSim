b43a22c09e0d897a4298fed5b6d3a53b6c25c4b7
Bourbouh <hamza.bourbouh@nasa.gov>
2018-08-14 17:25:07 -0700

    Fix Abs integer overflow

--- doc/cocosim_website/index.rst
+++ doc/cocosim_website/index.rst
@@ -10,6 +10,9 @@ Welcome to CoCoSim Documentation's documentation!
    :maxdepth: 2
    :caption: Contents:
 
+   usage/installation
+   usage/quickstart
+
 
 
 Indices and tables
--- src/middleEnd/lustre_multiclocks_compiler/blocks/Abs_To_Lustre.m
+++ src/middleEnd/lustre_multiclocks_compiler/blocks/Abs_To_Lustre.m
@@ -22,7 +22,7 @@ classdef Abs_To_Lustre < Block_To_Lustre
             outputDataType = blk.CompiledPortDataTypes.Outport{1};
             
             inputs{1} = SLX2LusUtils.getBlockInputsNames(parent, blk, 1);
-            inport_dt = blk.CompiledPortDataTypes.Inport(1);
+            inport_dt = blk.CompiledPortDataTypes.Inport{1};
             %converts the input data type(s) to
             %its accumulator data type
             if ~strcmp(inport_dt, outputDataType)
@@ -31,26 +31,48 @@ classdef Abs_To_Lustre < Block_To_Lustre
                 [external_lib, conv_format] = SLX2LusUtils.dataType_conversion(inport_dt, outputDataType, RndMeth, SaturateOnIntegerOverflow);
                 if ~isempty(external_lib)
                     obj.addExternal_libraries(external_lib);
-                    inputs{1} = cellfun(@(x) ...
-                        SLX2LusUtils.setArgInConvFormat(conv_format,x), inputs{1}, 'un', 0);
                 end
+            else
+                conv_format = {};
             end
             
             [~, zero] = SLX2LusUtils.get_lustre_dt(outputDataType);
             
-            codes = {};
+            
+            if contains(inport_dt, 'int')
+                n = 2;
+            else
+                n =1;
+            end
+            codes = cell(1, numel(inputs{1}));
             for j=1:numel(inputs{1})
-                code = IteExpr(...
-                    BinaryExpr(BinaryExpr.GTE, inputs{1}{j}, zero),...
-                    inputs{1}{j}, ...
-                    UnaryExpr(UnaryExpr.NEG, inputs{1}{j}));
+                % if the inport type is int8, int16 ... the absolute value of
+                % -128 for int8 is -128.
+                conds = cell(1, n);
+                thens = cell(1, n+1);
+                if MatlabUtils.startsWith(inport_dt, 'int')
+                    v_min = double(intmin(inport_dt));
+                    conds{1} = BinaryExpr(BinaryExpr.EQ, ...
+                        inputs{1}{j}, ...
+                        IntExpr(v_min));
+                    thens{1} = IntExpr(v_min);
+                end
+                conds{n} = BinaryExpr(BinaryExpr.GTE, ...
+                    inputs{1}{j}, zero);
+                thens{n} = inputs{1}{j};
+                thens{n+1} = UnaryExpr(UnaryExpr.NEG, ...
+                    inputs{1}{j});
+                code = ...
+                    SLX2LusUtils.setArgInConvFormat(...
+                    conv_format,...
+                    IteExpr.nestedIteExpr(conds, thens));
                 codes{j} = LustreEq(outputs{j}, code);
             end
             
             obj.setCode(codes);
         end
         
-        function options = getUnsupportedOptions(obj, parent, blk, varargin)
+        function options = getUnsupportedOptions(obj, ~, blk, varargin)
             obj.unsupported_options = {};
             if ~strcmp(blk.OutMax, '[]') || ~strcmp(blk.OutMin, '[]')
                 obj.addUnsupported_options(...
