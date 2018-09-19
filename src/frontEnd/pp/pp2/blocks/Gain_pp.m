ca50b91aee4e763297fca1d609a5d40b1f14ee93
ktrinh <khanh.v.trinh@nasa.gov>
2018-09-19 14:29:18 -0700

    Added try and catch in Gain

--- src/frontEnd/pp/pp2/blocks/Gain_pp.m
+++ src/frontEnd/pp/pp2/blocks/Gain_pp.m
@@ -1,4 +1,4 @@
-function [] = Gain_pp(model)
+function [status, errors_msg] = Gain_pp(model)
 % substitute_gain_process Searches for gain blocks and replaces them by a
 % PP-friendly equivalent.
 %   model is a string containing the name of the model to search in
@@ -9,42 +9,49 @@ function [] = Gain_pp(model)
 % Author: Hamza Bourbouh <hamza.bourbouh@nasa.gov>
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Processing Gain blocks
+status = 0;
+errors_msg = {};
+
 Gain_list = find_system(model, ...
     'LookUnderMasks', 'all', 'BlockType','Gain');
 if not(isempty(Gain_list))
     display_msg('Replacing Gain blocks...', MsgType.INFO,...
         'Gain_process', '');
     for i=1:length(Gain_list)
-        display_msg(Gain_list{i}, MsgType.INFO, ...
-            'Gain_process', '');
-        gain = get_param(Gain_list{i},'Gain');
-        CompiledPortDataTypes = SLXUtils.getCompiledParam(Gain_list{i}, 'CompiledPortDataTypes');
-        outputDataType = CompiledPortDataTypes.Outport{1}; %get_param(Gain_list{i}, 'OutDataTypeStr');
-        Multiplication = get_param(Gain_list{i}, 'Multiplication');
-        SaturateOnIntegerOverflow = get_param(Gain_list{i},'SaturateOnIntegerOverflow');
-        if strcmp(Multiplication, 'Element-wise(K.*u)')
-            pp_name = 'gain_ElementWise';
-        elseif strcmp(Multiplication, 'Matrix(K*u)') ...
-                || strcmp(Multiplication, 'Matrix(K*u) (u vector)')
-            pp_name = 'gain_K_U';
-        elseif strcmp(Multiplication, 'Matrix(u*K)')
-            pp_name = 'gain_U_K';
+        try
+            display_msg(Gain_list{i}, MsgType.INFO, ...
+                'Gain_process', '');
+            gain = get_param(Gain_list{i},'Gain');
+            CompiledPortDataTypes = SLXUtils.getCompiledParam(Gain_list{i}, 'CompiledPortDataTypes');
+            outputDataType = CompiledPortDataTypes.Outport{1}; %get_param(Gain_list{i}, 'OutDataTypeStr');
+            Multiplication = get_param(Gain_list{i}, 'Multiplication');
+            SaturateOnIntegerOverflow = get_param(Gain_list{i},'SaturateOnIntegerOverflow');
+            if strcmp(Multiplication, 'Element-wise(K.*u)')
+                pp_name = 'gain_ElementWise';
+            elseif strcmp(Multiplication, 'Matrix(K*u)') ...
+                    || strcmp(Multiplication, 'Matrix(K*u) (u vector)')
+                pp_name = 'gain_K_U';
+            elseif strcmp(Multiplication, 'Matrix(u*K)')
+                pp_name = 'gain_U_K';
+            end
+            replace_one_block(Gain_list{i},fullfile('pp_lib',pp_name));
+            set_param(strcat(Gain_list{i},'/K'),...
+                'Value',gain);
+            set_param(strcat(Gain_list{i},'/K'),...
+                'OutDataTypeStr','Inherit: Inherit via back propagation');
+ 
+            set_param(strcat(Gain_list{i},'/Product'),...
+                'OutDataTypeStr',outputDataType);
+            set_param(strcat(Gain_list{i},'/Product'),...
+                'SaturateOnIntegerOverflow',SaturateOnIntegerOverflow);
+        catch
+            status = 1;
+            errors_msg{end + 1} = sprintf('Gain pre-process has failed for block %s', Gain_list{i});
+            continue;
         end
-        replace_one_block(Gain_list{i},fullfile('pp_lib',pp_name));
-        set_param(strcat(Gain_list{i},'/K'),...
-            'Value',gain);
-        set_param(strcat(Gain_list{i},'/K'),...
-            'OutDataTypeStr','Inherit: Inherit via back propagation');
-        
-%         if strcmp(outputDataType, 'Inherit: Same as input')
-%             outputDataType = 'Inherit: Same as first input';
-%         end
-        set_param(strcat(Gain_list{i},'/Product'),...
-            'OutDataTypeStr',outputDataType);
-        set_param(strcat(Gain_list{i},'/Product'),...
-            'SaturateOnIntegerOverflow',SaturateOnIntegerOverflow);
     end
     display_msg('Done\n\n', MsgType.INFO, 'Gain_process', '');
 end
+
 end
 
--- src/middleEnd/lustre_multiclocks_compiler/frontEnd/SS_To_LustreNode.m
+++ src/middleEnd/lustre_multiclocks_compiler/frontEnd/SS_To_LustreNode.m
@@ -186,7 +186,15 @@ classdef SS_To_LustreNode
                 if status
                     continue;
                 end
-                b.write_code(subsys, blk, xml_trace, main_sampleTime, backend);
+                try
+                    b.write_code(subsys, blk, xml_trace, main_sampleTime, backend);
+                catch me
+                    display_msg(sprintf('Translation to Lustre of block %s has failed.', blk.Origin_path),...
+                        MsgType.ERROR, 'write_body', '');
+                    display_msg(me.getReport(), MsgType.DEBUG, 'write_body', '');
+                end
+                    
+                    
                 if iscell(b.getCode())
                     body = [body, b.getCode()];
                 else
