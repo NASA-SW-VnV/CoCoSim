1ae3f9ff0cacfd34e4f68ae762ea1d183e0fc32d
Bourbouh <hamza.bourbouh@nasa.gov>
2018-12-05 01:05:03 -0800

    improve performance

--- cocosim_config.m
+++ cocosim_config.m
@@ -4,67 +4,79 @@
 % All Rights Reserved.
 % Author: Hamza Bourbouh <hamza.bourbouh@nasa.gov>
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% add paths
-[cocoSim_root, ~, ~] = fileparts(mfilename('fullpath'));
-warning off
-%add all folders except tools sub-folders
-addpath(genpath(cocoSim_root));
-rmpath(genpath(fullfile(cocoSim_root, 'tools')));
-%add only tools not its sub-folders
-addpath(fullfile(cocoSim_root, 'tools'));
-% addpath(genpath(fullfile(cocoSim_root, 'libs')));
-% addpath(genpath(fullfile(cocoSim_root, 'scripts')));
-% addpath(genpath(fullfile(cocoSim_root, 'src')));
-% addpath(fullfile(cocoSim_root, 'tools'));
-
-%% First configuration, Zustre, Kind2 and Lustrec
-% Go to tools/tools_config and follow instructions
-install_cocosim_lib();
-tools_config;
-
-
-%% Second configuration Pre-processing
-% Go to src/pp/pp_config and follow instructions
-pp_config;
-fprintf('\n\t Click <a href="matlab: pp_user_config">here</a> to change pre-processing configuration.\n');
-
-%% IR config
-
-ir_utils_path = fullfile(cocoSim_root, 'src', 'frontEnd', 'IR', 'utils');
-json_encode_file = 'json_encode';
-json_decode_file = 'json_decode';
-
-if ismac
-    json_encode_file = fullfile(ir_utils_path, 'json_encode.mexmaci64');
-    json_decode_file = fullfile(ir_utils_path, 'json_decode.mexmaci64');
-elseif isunix
-    json_encode_file = fullfile(ir_utils_path, 'json_encode.mexa64');
-    json_decode_file = fullfile(ir_utils_path, 'json_decode.mexa64');
-elseif ispc
-    json_encode_file = fullfile(ir_utils_path, 'json_encode.mexw64');
-    json_decode_file = fullfile(ir_utils_path, 'json_decode.mexw64');
+% for compatibility with old cocosim libraries, we add this global
+% variable.
+global cocosim_config_already_run;
+if isempty(cocosim_config_already_run)
+    cocosim_config_already_run = 0;
+else
+    cocosim_config_already_run = 1;
 end
-
-if ~ exist(json_encode_file, 'file') || ~ exist(json_decode_file, 'file')
-    if exist(fullfile(ir_utils_path, 'make.m'), 'file')
-        PWD = pwd;
-        cd(ir_utils_path);
-        try
-            make
-        catch  ME
-            display_msg(ME.getReport(), MsgType.ERROR, 'cocosim_config', '');
-        end
-        cd(PWD);
+if cocosim_config_already_run
+    % only tools_config is needed from old compiler
+    tools_config;
+else
+    %% add paths
+    [cocoSim_root, ~, ~] = fileparts(mfilename('fullpath'));
+    warning off
+    %add all folders except tools sub-folders
+    addpath(genpath(cocoSim_root));
+    rmpath(genpath(fullfile(cocoSim_root, 'tools')));
+    %add only tools not its sub-folders
+    addpath(fullfile(cocoSim_root, 'tools'));
+    % addpath(genpath(fullfile(cocoSim_root, 'libs')));
+    % addpath(genpath(fullfile(cocoSim_root, 'scripts')));
+    % addpath(genpath(fullfile(cocoSim_root, 'src')));
+    % addpath(fullfile(cocoSim_root, 'tools'));
+    
+    %% First configuration, Zustre, Kind2 and Lustrec
+    % Go to tools/tools_config and follow instructions
+    install_cocosim_lib();
+    tools_config;
+    
+    
+    %% Second configuration Pre-processing
+    % Go to src/pp/pp_config and follow instructions
+    pp_config;
+    fprintf('\n\t Click <a href="matlab: pp_user_config">here</a> to change pre-processing configuration.\n');
+    
+    %% IR config
+    
+    ir_utils_path = fullfile(cocoSim_root, 'src', 'frontEnd', 'IR', 'utils');
+    json_encode_file = 'json_encode';
+    json_decode_file = 'json_decode';
+    
+    if ismac
+        json_encode_file = fullfile(ir_utils_path, 'json_encode.mexmaci64');
+        json_decode_file = fullfile(ir_utils_path, 'json_decode.mexmaci64');
+    elseif isunix
+        json_encode_file = fullfile(ir_utils_path, 'json_encode.mexa64');
+        json_decode_file = fullfile(ir_utils_path, 'json_decode.mexa64');
+    elseif ispc
+        json_encode_file = fullfile(ir_utils_path, 'json_encode.mexw64');
+        json_decode_file = fullfile(ir_utils_path, 'json_decode.mexw64');
     end
+    
+    if ~ exist(json_encode_file, 'file') || ~ exist(json_decode_file, 'file')
+        if exist(fullfile(ir_utils_path, 'make.m'), 'file')
+            PWD = pwd;
+            cd(ir_utils_path);
+            try
+                make
+            catch  ME
+                display_msg(ME.getReport(), MsgType.ERROR, 'cocosim_config', '');
+            end
+            cd(PWD);
+        end
+    end
+    
+    %% Java external libraries
+    matlabParser = fullfile(cocoSim_root, 'src','frontEnd', 'IR',...
+        'Matlab_IR', 'Matlab-Parser.jar');
+    
+    if exist(matlabParser, 'file')
+        javaaddpath(matlabParser);
+    end
+    
+    warning on
 end
-
-%% Java external libraries
-matlabParser = fullfile(cocoSim_root, 'src','frontEnd', 'IR',...
-    'Matlab_IR', 'Matlab-Parser.jar');
-
-if exist(matlabParser, 'file')
-    javaaddpath(matlabParser);
-end
-
-warning on
--- tools/tools_config.m
+++ tools/tools_config.m
@@ -24,121 +24,126 @@
 
 global tools_config_already_run LUSTREC LUSTREC_OPTS LUSTRET ...
     LUCTREC_INCLUDE_DIR ZUSTRE Z3 KIND2 JKIND SEAHORN...
-    WLLVM WLLVMPP EXTRACT_BC IKOS;
+    WLLVM WLLVMPP EXTRACT_BC IKOS cocosim_version;
 if isempty(tools_config_already_run)
     tools_config_already_run = 0;
 else
     tools_config_already_run = 1;
 end
-[tools_root, ~, ~] = fileparts(which('tools_config')); %fileparts(mfilename('fullpath'));
-cocoSim_root = fileparts(tools_root);
-if ~exist('solvers_path', 'var')
-    solvers_path = fullfile(cocoSim_root, 'tools', 'verifiers');
-    if ismac
-        solvers_path = fullfile(solvers_path, 'osx');
-        Z3Library_path = fullfile(solvers_path,'spacer', 'lib', 'libz3.dylib');
-        LD_LIBRARY_PATH = 'DYLD_LIBRARY_PATH';
-        
-    elseif isunix
-        solvers_path = fullfile(solvers_path, 'linux');
-        Z3Library_path = fullfile(solvers_path,'spacer', 'lib', 'libz3.so');
-        LD_LIBRARY_PATH = 'LD_LIBRARY_PATH';
-    elseif ispc
-        Z3Library_path = fullfile(cocosim_path, 'tools\verifiers\Z3\bin\libz3.dll');
-    else
-        errordlg('OS not supported yet','CoCoSim backend configuration');
-    end
-    OldLibPath = getenv(LD_LIBRARY_PATH);
-    if ~contains(OldLibPath,Z3Library_path)
-        setenv(LD_LIBRARY_PATH,[OldLibPath ':' Z3Library_path]);
-    end
-    oldPythonPath = getenv('PYTHONPATH');
-    z3PythonPath = fullfile(solvers_path,'spacer', 'lib', 'python2.7','dist-packages');
-    if ~contains(oldPythonPath, z3PythonPath)
-        setenv('PYTHONPATH', [z3PythonPath ':' oldPythonPath]);
-    end
-end
-
-LUSTREC = fullfile(solvers_path, 'bin', 'lustrec');
-%LUSTREC_OPTS defines options that should be passed to Lustrec when it is
-%called. e.g. "int_div_euclidean" interprets integer division as Euclidean 
-%(default : C division semantics)
-LUSTREC_OPTS = '-int_div_euclidean -algebraic-loop-solve -int "long long int"';
-LUSTRET = fullfile(solvers_path, 'bin', 'lustret');
-LUCTREC_INCLUDE_DIR = fullfile(solvers_path, 'include', 'lustrec');
-ZUSTRE = fullfile(solvers_path, 'bin', 'zustre');
-Z3 = fullfile(solvers_path,'spacer', 'bin', 'z3');
-KIND2 = fullfile(solvers_path, 'bin', 'kind2');
-JKIND = 'Path to Jkind binary';
-SEAHORN = 'PATH';
-
-if ~tools_config_already_run
-    if ~exist(LUSTREC,'file')
-        warning('LUSTREC is not found in %s, configure your path in tools_config.m', LUSTREC);
-        warning('Please Ignore the previous warning if you are not going to use Zustre or Compiler Validation');
-    end
-    if ~exist(ZUSTRE,'file')
-        warning('Zustre is not found in %s, configure your path in tools_config.m', ZUSTRE);
-        warning('Please Ignore the previous warning if you are not going to use Zustre for verification');
-    end
-    if ~exist(Z3,'file')
-        warning('Z3 is not found in %s, configure your path in tools_config.m', Z3);
-        warning('Please Ignore the previous warning if you are not going to use Zustre ');
-    end
-    if ~exist(KIND2,'file')
-        warning('KIND2 is not found in %s, configure your path in tools_config.m', KIND2);
-        warning('Please Ignore the previous warning if you are not going to use Kind2 for verification');
+if tools_config_already_run && ~isempty(LUSTREC) && ~isempty(KIND2) && ~isempty(Z3)
+    %already run
+else
+    
+    [tools_root, ~, ~] = fileparts(which('tools_config')); %fileparts(mfilename('fullpath'));
+    cocoSim_root = fileparts(tools_root);
+    if ~exist('solvers_path', 'var')
+        solvers_path = fullfile(cocoSim_root, 'tools', 'verifiers');
+        if ismac
+            solvers_path = fullfile(solvers_path, 'osx');
+            Z3Library_path = fullfile(solvers_path,'spacer', 'lib', 'libz3.dylib');
+            LD_LIBRARY_PATH = 'DYLD_LIBRARY_PATH';
+            
+        elseif isunix
+            solvers_path = fullfile(solvers_path, 'linux');
+            Z3Library_path = fullfile(solvers_path,'spacer', 'lib', 'libz3.so');
+            LD_LIBRARY_PATH = 'LD_LIBRARY_PATH';
+        elseif ispc
+            Z3Library_path = fullfile(cocosim_path, 'tools\verifiers\Z3\bin\libz3.dll');
+        else
+            errordlg('OS not supported yet','CoCoSim backend configuration');
+        end
+        OldLibPath = getenv(LD_LIBRARY_PATH);
+        if ~contains(OldLibPath,Z3Library_path)
+            setenv(LD_LIBRARY_PATH,[OldLibPath ':' Z3Library_path]);
+        end
+        oldPythonPath = getenv('PYTHONPATH');
+        z3PythonPath = fullfile(solvers_path,'spacer', 'lib', 'python2.7','dist-packages');
+        if ~contains(oldPythonPath, z3PythonPath)
+            setenv('PYTHONPATH', [z3PythonPath ':' oldPythonPath]);
+        end
     end
     
-end
-%% IKOS Configuration: WLLVM, WLLVMPP, EXTRACT_BC, IKOS
-% You should be interested to configure this part only if you will use IKOS
-% Install a tool called **Whole Program LLVM**: https://github.com/travitch/whole-program-llvm
-% You can install it easily using pip:
-% ```
-% $ pip install wllvm
-% ```
-
-% put Clang path here
-[status, CLANG_PATH] = system('which clang');
-if isempty(status) || status~=0 || isempty(CLANG_PATH) || isnumeric(CLANG_PATH)
-    CLANG_PATH = 'Path_to_llvm/bin/clang';
-end
-setenv('PATH',[CLANG_PATH ':' getenv('PATH')  ])
-
-% wllvm path should contains wllvm wllvm++ and extract-bc
-[status, WLLVM_PATH] = system('which wllvm');
-if isempty(status) || status~=0 || isempty(WLLVM_PATH) || isnumeric(WLLVM_PATH)
-    % put your wllvm path here instead of mine
-    WLLVM_PATH = 'path_to_WLLVM';
-end
-
-WLLVM = fullfile(WLLVM_PATH,'wllvm');
-WLLVMPP = fullfile(WLLVM_PATH,'wllvm++');
-EXTRACT_BC = fullfile(WLLVM_PATH,'extract-bc');
-[status, IKOS] = system('which ikos');
-if isempty(status) || status~=0 || isempty(IKOS) || isnumeric(IKOS)
-    IKOS = 'path_to_IKOS';
-end
-
-
-if ~tools_config_already_run
-    if ~exist(WLLVM,'file')
-        warning('WLLVM is not found in %s, configure your path in config.m', WLLVM);
-        warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+    LUSTREC = fullfile(solvers_path, 'bin', 'lustrec');
+    %LUSTREC_OPTS defines options that should be passed to Lustrec when it is
+    %called. e.g. "int_div_euclidean" interprets integer division as Euclidean
+    %(default : C division semantics)
+    LUSTREC_OPTS = '-int_div_euclidean -algebraic-loop-solve -int "long long int"';
+    LUSTRET = fullfile(solvers_path, 'bin', 'lustret');
+    LUCTREC_INCLUDE_DIR = fullfile(solvers_path, 'include', 'lustrec');
+    ZUSTRE = fullfile(solvers_path, 'bin', 'zustre');
+    Z3 = fullfile(solvers_path,'spacer', 'bin', 'z3');
+    KIND2 = fullfile(solvers_path, 'bin', 'kind2');
+    JKIND = 'Path to Jkind binary';
+    SEAHORN = 'PATH';
+    
+    if ~tools_config_already_run
+        if ~exist(LUSTREC,'file')
+            warning('LUSTREC is not found in %s, configure your path in tools_config.m', LUSTREC);
+            warning('Please Ignore the previous warning if you are not going to use Zustre or Compiler Validation');
+        end
+        if ~exist(ZUSTRE,'file')
+            warning('Zustre is not found in %s, configure your path in tools_config.m', ZUSTRE);
+            warning('Please Ignore the previous warning if you are not going to use Zustre for verification');
+        end
+        if ~exist(Z3,'file')
+            warning('Z3 is not found in %s, configure your path in tools_config.m', Z3);
+            warning('Please Ignore the previous warning if you are not going to use Zustre ');
+        end
+        if ~exist(KIND2,'file')
+            warning('KIND2 is not found in %s, configure your path in tools_config.m', KIND2);
+            warning('Please Ignore the previous warning if you are not going to use Kind2 for verification');
+        end
+        
     end
-    if ~exist(WLLVMPP,'file')
-        warning('WLLVMPP is not found in %s, configure your path in config.m', WLLVMPP);
-        warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+    %% IKOS Configuration: WLLVM, WLLVMPP, EXTRACT_BC, IKOS
+    % You should be interested to configure this part only if you will use IKOS
+    % Install a tool called **Whole Program LLVM**: https://github.com/travitch/whole-program-llvm
+    % You can install it easily using pip:
+    % ```
+    % $ pip install wllvm
+    % ```
+    
+    % put Clang path here
+    [status, CLANG_PATH] = system('which clang');
+    if isempty(status) || status~=0 || isempty(CLANG_PATH) || isnumeric(CLANG_PATH)
+        CLANG_PATH = 'Path_to_llvm/bin/clang';
     end
-    if ~exist(EXTRACT_BC,'file')
-        warning('EXTRACT_BC is not found in %s, configure your path in config.m', EXTRACT_BC);
-        warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+    setenv('PATH',[CLANG_PATH ':' getenv('PATH')  ])
+    
+    % wllvm path should contains wllvm wllvm++ and extract-bc
+    [status, WLLVM_PATH] = system('which wllvm');
+    if isempty(status) || status~=0 || isempty(WLLVM_PATH) || isnumeric(WLLVM_PATH)
+        % put your wllvm path here instead of mine
+        WLLVM_PATH = 'path_to_WLLVM';
     end
-    if ~exist(IKOS,'file')
-        warning('IKOS is not found in %s, configure your path in config.m', IKOS);
-        warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+    
+    WLLVM = fullfile(WLLVM_PATH,'wllvm');
+    WLLVMPP = fullfile(WLLVM_PATH,'wllvm++');
+    EXTRACT_BC = fullfile(WLLVM_PATH,'extract-bc');
+    [status, IKOS] = system('which ikos');
+    if isempty(status) || status~=0 || isempty(IKOS) || isnumeric(IKOS)
+        IKOS = 'path_to_IKOS';
     end
+    
+    
+    if ~tools_config_already_run
+        if ~exist(WLLVM,'file')
+            warning('WLLVM is not found in %s, configure your path in config.m', WLLVM);
+            warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+        end
+        if ~exist(WLLVMPP,'file')
+            warning('WLLVMPP is not found in %s, configure your path in config.m', WLLVMPP);
+            warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+        end
+        if ~exist(EXTRACT_BC,'file')
+            warning('EXTRACT_BC is not found in %s, configure your path in config.m', EXTRACT_BC);
+            warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+        end
+        if ~exist(IKOS,'file')
+            warning('IKOS is not found in %s, configure your path in config.m', IKOS);
+            warning('Please Ignore the previous warning if you are not going to use IKOS for analyzing C code');
+        end
+    end
+    %%
+    cocosim_version = 'v0.1';
 end
-%%
-cocosim_version = 'v0.1';
