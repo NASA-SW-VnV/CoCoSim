02766a604a2e3c58536084021c6643db4422e19e
ktrinh <khanh.v.trinh@nasa.gov>
2019-02-11 15:05:06 -0800

    class folder for Chart_To_Lustre.m

--- src/middleEnd/+nasa_toLustre/+blocks/@Chart_To_Lustre/Chart_To_Lustre.m
+++ src/middleEnd/+nasa_toLustre/+blocks/@Chart_To_Lustre/Chart_To_Lustre.m
@@ -205,26 +205,7 @@ classdef Chart_To_Lustre < nasa_toLustre.frontEnd.Block_To_Lustre
         end
     end
     methods(Static)
-        function transitions = getAllTransitions(SFContent)
-            import nasa_toLustre.blocks.Chart_To_Lustre
-            transitions = {};
-            for i=1:numel(SFContent.States)
-                transitions = [transitions, ...
-                    SFContent.States{i}.Composition.DefaultTransitions];
-                transitions = [transitions, ...
-                    SFContent.States{i}.OuterTransitions];
-                transitions = [transitions, ...
-                    SFContent.States{i}.InnerTransitions];
-            end
-            for i=1:numel(SFContent.Junctions)
-                transitions = [transitions, ...
-                    SFContent.Junctions{i}.OuterTransitions];
-            end
-            for i=1:numel(SFContent.GraphicalFunctions)
-                transitions = [transitions, ...
-                    Chart_To_Lustre.getAllTransitions(SFContent.GraphicalFunctions{i})];
-            end
-        end
+        transitions = getAllTransitions(SFContent)
     end
 end
 
