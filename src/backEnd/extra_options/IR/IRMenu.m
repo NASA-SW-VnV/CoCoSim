6e5cac48ba63acb74c8e9c879f7ae06142dcfbd8
ktrinh <khanh.v.trinh@nasa.gov>
2019-02-13 14:13:19 -0800

    added coppyright to IRMenu.m contractToSLDVMenu.m transformContractToSLDV.m ../extra_options/pp/ppMenu.m validationMenu.m

--- src/backEnd/extra_options/IR/IRMenu.m
+++ src/backEnd/extra_options/IR/IRMenu.m
@@ -7,24 +7,24 @@
 
 
 function schema = IRMenu(callbackInfo)
-schema = sl_action_schema;
-schema.label = 'Export model to JSON format';
-schema.callback = @IRCallback;
+    schema = sl_action_schema;
+    schema.label = 'Export model to JSON format';
+    schema.callback = @IRCallback;
 end
 
 
 function IRCallback(callbackInfo)
-try
-    model_full_path = MenuUtils.get_file_name(gcs) ;
-    cocosim_IR( model_full_path, 1 );
-    
-    [parent, file_name, ~] = fileparts(model_full_path);
-    json_path = fullfile(parent, [file_name '_IR.json']);
-    display_msg(['JSON file is : ', json_path], ...
-        MsgType.RESULT, 'IRMenu','');
-catch ME
-    display_msg(ME.getReport(), MsgType.DEBUG,'IRMenu','');
-    display_msg(ME.message, MsgType.ERROR,'IRMenu','');
-end
+    try
+        model_full_path = MenuUtils.get_file_name(gcs) ;
+        cocosim_IR( model_full_path, 1 );
+
+        [parent, file_name, ~] = fileparts(model_full_path);
+        json_path = fullfile(parent, [file_name '_IR.json']);
+        display_msg(['JSON file is : ', json_path], ...
+            MsgType.RESULT, 'IRMenu','');
+    catch ME
+        display_msg(ME.getReport(), MsgType.DEBUG,'IRMenu','');
+        display_msg(ME.message, MsgType.ERROR,'IRMenu','');
+    end
 end
 
