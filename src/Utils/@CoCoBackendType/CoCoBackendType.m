efb535ae1d9d3e13d49bf7f4e640aa2c153ca523
ktrinh <khanh.v.trinh@nasa.gov>
2019-02-13 18:26:22 -0800

    class folder for CoCoBackendType.m and some clean up

---
+++ src/Utils/@CoCoBackendType/CoCoBackendType.m
@@ -0,0 +1,34 @@
+classdef CoCoBackendType < handle
+    %Backend Types
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Copyright (c) 2017 United States Government as represented by the
+    % Administrator of the National Aeronautics and Space Administration.
+    % All Rights Reserved.
+    % Author: Hamza Bourbouh <hamza.bourbouh@nasa.gov>
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    properties (Constant)
+        % CoCoSim Backends
+        COMPATIBILITY = 'COMPATIBILITY';
+        VERIFICATION = 'Verification';
+        VALIDATION = 'Validation';
+        GUIDELINES = 'GUIDELINES';
+        DED = 'DesignErrorDetection';
+        DED_INTOVERFLOW = 'Integer Overflow';
+        DED_DIVBYZER = 'Division By Zero';
+        DED_OUTMINMAX = 'Check OutMin OutMax';
+        DED_OUTOFBOUND = 'Out of Bound Array Access';
+    end
+    
+    methods(Static)
+        res = isCOMPATIBILITY(b)
+
+        res = isVERIFICATION(b)
+
+        res = isVALIDATION(b)
+
+        res = isGUIDELINES(b)
+
+        res = isDED(b)
+
+    end
+end
