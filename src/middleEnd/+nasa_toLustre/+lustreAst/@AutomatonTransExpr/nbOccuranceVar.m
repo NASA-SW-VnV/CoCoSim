fbd8e73f8be14c075f92de1db467e18aeb8ff86c
ktrinh <khanh.v.trinh@nasa.gov>
2019-02-14 18:50:30 -0800

    class folder for AutomatonTransExpr.m

---
+++ src/middleEnd/+nasa_toLustre/+lustreAst/@AutomatonTransExpr/nbOccuranceVar.m
@@ -0,0 +1,10 @@
+function nb_occ = nbOccuranceVar(varargin)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2017 United States Government as represented by the
+% Administrator of the National Aeronautics and Space Administration.
+% All Rights Reserved.
+% Author: Hamza Bourbouh <hamza.bourbouh@nasa.gov>
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    nb_occ = 0;
+end
---
+++ src/middleEnd/+nasa_toLustre/+lustreAst/@AutomatonTransExpr/pseudoCode2Lustre.m
@@ -0,0 +1,11 @@
+function [new_obj, outputs_map] = pseudoCode2Lustre(obj, outputs_map, isLeft)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2017 United States Government as represented by the
+% Administrator of the National Aeronautics and Space Administration.
+% All Rights Reserved.
+% Author: Hamza Bourbouh <hamza.bourbouh@nasa.gov>
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    %TODO: Not done for this class yet, as it is not used by stateflow.
+    new_obj = obj;
+end
