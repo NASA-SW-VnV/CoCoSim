a91ffe67557fcba1120776367cb547cebc9c5d6a
ktrinh <khanh.v.trinh@nasa.gov>
2019-02-14 21:33:32 -0800

    class folder for ContractImportExpr.m

---
+++ src/middleEnd/+nasa_toLustre/+lustreAst/@ContractImportExpr/print_kind2.m
@@ -0,0 +1,13 @@
+function code = print_kind2(obj, backend)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2017 United States Government as represented by the
+% Administrator of the National Aeronautics and Space Administration.
+% All Rights Reserved.
+% Author: Hamza Bourbouh <hamza.bourbouh@nasa.gov>
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    inputs_str = nasa_toLustre.lustreAst.NodeCallExpr.getArgsStr(obj.inputs, backend);
+    outputs_str = nasa_toLustre.lustreAst.NodeCallExpr.getArgsStr(obj.outputs, backend);
+    code = sprintf('import %s(%s) returns (%s);', ...
+        obj.name, inputs_str, outputs_str );
+end
