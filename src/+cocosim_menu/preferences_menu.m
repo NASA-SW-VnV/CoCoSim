43d7d0a52cd3df06c7a15a2178178135c03ca973
hbourbou <hamza.bourbouh@nasa.gov>
2019-02-21 19:59:31 -0800

    more fixes

--- src/+cocosim_menu/preferences_menu.m
+++ src/+cocosim_menu/preferences_menu.m
@@ -17,8 +17,7 @@ function schema = preferences_menu(callbackInfo)
     schema.childrenFcns = {...
         {@getLustreCompiler, CoCoSimPreferences}, ...
         {@getLustreBackend, CoCoSimPreferences}, ...
-        {@PreferencesMenu.getCompositionalAnalysis, CoCoSimPreferences}, ...
-        {@PreferencesMenu.getKind2Binary, CoCoSimPreferences}, ...
+        {@getKind2Options, CoCoSimPreferences}, ...
         {@PreferencesMenu.getVerificationTimeout, CoCoSimPreferences}, ...
         {@getDEDChecks, CoCoSimPreferences}...
         };
@@ -27,7 +26,7 @@ end
 %% Lustre compiler
 function schema = getLustreCompiler(callbackInfo)
     schema = sl_container_schema;
-    schema.label = 'Lustre compiler';
+    schema.label = 'Simulink To Lustre compiler';
     schema.statustip = 'Lustre compiler';
     schema.autoDisableWhen = 'Busy';
     CoCoSimPreferences = callbackInfo.userdata;
@@ -62,7 +61,7 @@ end
 %% Lustre Backend
 function schema = getLustreBackend(callbackInfo)
     schema = sl_container_schema;
-    schema.label = 'Lustre Backend';
+    schema.label = 'Lustre Verification Backend';
     schema.statustip = 'Lustre backend';
     schema.autoDisableWhen = 'Busy';
     CoCoSimPreferences = callbackInfo.userdata;
@@ -94,6 +93,20 @@ function setBackendOption(backendName, CoCoSimPreferences, varargin)
     PreferencesMenu.saveCoCoSimPreferences(CoCoSimPreferences);
 end
 
+%% Kind2 options
+function schema = getKind2Options(callbackInfo)
+    schema = sl_container_schema;
+    schema.label = 'Kind2 Preferences';
+    schema.statustip = 'Kind2 Preferences';
+    schema.autoDisableWhen = 'Busy';
+    
+    CoCoSimPreferences = callbackInfo.userdata;
+    
+    schema.childrenFcns = {...
+        {@PreferencesMenu.getCompositionalAnalysis, CoCoSimPreferences}, ...
+        {@PreferencesMenu.getKind2Binary, CoCoSimPreferences}
+        };
+end
 %% DED Checks
 function schema = getDEDChecks(callbackInfo)
     schema = sl_container_schema;
