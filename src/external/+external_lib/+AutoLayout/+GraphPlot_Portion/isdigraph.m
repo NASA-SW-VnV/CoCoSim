96e656a33207db5d30952fcb60053fa6b3502b4a
hbourbou <hamza.bourbouh@nasa.gov>
2019-04-26 16:53:36 -0700

    delete external libraries

--- src/external/+external_lib/+AutoLayout/+GraphPlot_Portion/isdigraph.m
+++
@@ -1,5 +0,0 @@
-function tf = isdigraph(d)
-% ISDIGRAPH Return true (1) if d is a digraph and false (0) otherwise.
-
-    tf = isa(d, 'digraph');
-end
--- src/external/+external_lib/+AutoLayout/+Graphviz_Portion/autoLayout.bat
+++
@@ -1,4 +0,0 @@
-for %%f in (*.dot) do (
-            dot "%%~nf.dot" -Tplain -o "%%~nf-plain.txt"
-            dot "%%~nf.dot" -Tpdf -o "%%~nf.pdf"
-)
