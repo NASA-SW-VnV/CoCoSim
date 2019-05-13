e91b4b532e07abaa28fa7f98f40e3f43aca2a153
hbourbou <hamza.bourbouh@nasa.gov>
2019-05-13 16:35:13 -0700

    few changes in docker

--- src/utils/CoCoDocker.m
+++ src/utils/CoCoDocker.m
@@ -5,15 +5,26 @@ classdef CoCoDocker
     end
     
     methods(Static)
-        function c_name = getCurrentContainerName(restart)
+        function c_name = getCurrentContainerName(restart, kill)
             persistent cname;
             if isempty(cname)
                 cname = '';
             end
+            
+            if nargin < 1
+                restart = false;
+            end
+            if nargin < 2
+                kill = false;
+            end
+            
             if restart
                 cname = strcat('c', strrep(num2str(rand(1)), '.', ''));
             end
             c_name = cname;
+            if kill
+                cname = '';
+            end
         end
         
         function [c_name, errCode, stdout] = start(sharedFolder)
@@ -21,6 +32,18 @@ classdef CoCoDocker
             [errCode, stdout] = system(sprintf('docker run -d -name=%s -v %s:/lus cocosim/lustre', ...
                 c_name, sharedFolder));
         end
+        
+        function [errCode, stdout] = cp(dockerPath, hostPath)
+            c_name = CoCoDocker.getCurrentContainerName();
+            [errCode, stdout] = system(sprintf('docker cp %s:%s %s', ...
+                c_name, dockerPath, hostPath));
+        end 
+        
+        function [errCode, stdout] = stop()
+            c_name = CoCoDocker.getCurrentContainerName(false, true);
+            [errCode, stdout] = system(sprintf('docker stop %s; docker rm %s', ...
+                c_name, c_name));
+        end 
         
         function [errCode, stdout] = exec(sharedFolder, cmd)
             c_name = CoCoDocker.getCurrentContainerName();
@@ -34,17 +57,7 @@ classdef CoCoDocker
                 c_name, cmd));
         end
         
-        function [errCode, stdout] = cp(dockerPath, hostPath)
-            c_name = CoCoDocker.getCurrentContainerName();
-            [errCode, stdout] = system(sprintf('docker cp %s:%s %s', ...
-                c_name, dockerPath, hostPath));
-        end 
         
-        function [errCode, stdout] = stop()
-            c_name = CoCoDocker.getCurrentContainerName();
-            [errCode, stdout] = system(sprintf('docker stop %s; docker rm %s', ...
-                c_name, c_name));
-        end 
     end
 end
 
