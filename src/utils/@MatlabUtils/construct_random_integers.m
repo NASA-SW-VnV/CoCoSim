1552e6357fd9e7e1821a19b72fa8e555f900e9ac
hbourbou <hamza.bourbouh@nasa.gov>
2019-05-15 18:11:03 -0700

    fix random integers generator

--- src/utils/@MatlabUtils/construct_random_integers.m
+++ src/utils/@MatlabUtils/construct_random_integers.m
@@ -7,8 +7,8 @@
  
 function vector = construct_random_integers(nb_iterations, IMIN, IMAX, dt, dim)
     if numel(dim)==1
-        vector = randi([IMIN, IMAX], [nb_iterations,dim],dt);
+        vector = randi(floor([IMIN, IMAX]), [nb_iterations,dim],dt);
     else
-        vector = randi([IMIN, IMAX], [dim,nb_iterations],dt);
+        vector = randi(floor([IMIN, IMAX]), [dim,nb_iterations],dt);
     end
 end
