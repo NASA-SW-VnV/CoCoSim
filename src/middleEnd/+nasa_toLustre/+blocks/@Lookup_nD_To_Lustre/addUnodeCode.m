1f5a8598f827f564e2f788f0ab972d116a93b05c
Hamza Bourbouh <bourbouhh@gmail.com>
2019-05-27 16:26:32 -0700

    add binarysearch method for lookuptable

--- src/middleEnd/+nasa_toLustre/+blocks/@Lookup_nD_To_Lustre/addUnodeCode.m
+++ src/middleEnd/+nasa_toLustre/+blocks/@Lookup_nD_To_Lustre/addUnodeCode.m
@@ -6,7 +6,7 @@ function [body, vars,u_node] = addUnodeCode(...
     % All Rights Reserved.
     % Author: Trinh, Khanh V <khanh.v.trinh@nasa.gov>
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+    
     % This function defines and calculating shape function values for the
     % interpolation point
     body = {};   % body may grow if ~directLookup
@@ -14,26 +14,33 @@ function [body, vars,u_node] = addUnodeCode(...
     u_node = {};
     
     numBoundNodes = 2^blkParams.NumberOfTableDimensions;
-    for i=1:numBoundNodes
-        if ~blkParams.directLookup
-            u_node{i} = nasa_toLustre.lustreAst.VarIdExpr(...
-                sprintf('u_node_%d',i));
-            vars{end+1} = nasa_toLustre.lustreAst.LustreVar(...
-                u_node{i},'real');              
-            % defining u_node{i}
-            conds = cell(1,numel(table_elem)-1);
-            thens = cell(1,numel(table_elem));
-            for j=1:numel(table_elem)-1                
-                conds{j} = nasa_toLustre.lustreAst.BinaryExpr(...
-                    nasa_toLustre.lustreAst.BinaryExpr.EQ,...
-                    boundingi{i},nasa_toLustre.lustreAst.IntExpr(j));
-                thens{j} = table_elem{j};                
-            end
-            thens{numel(table_elem)} = table_elem{numel(table_elem)};
-            rhs = nasa_toLustre.lustreAst.IteExpr.nestedIteExpr(conds, thens);
-            body{end+1} = nasa_toLustre.lustreAst.LustreEq(u_node{i},rhs);          
+    if ~blkParams.directLookup
+        for i=1:numBoundNodes
+            vName = sprintf('u_node_%d',i);
+            u_node{i} = nasa_toLustre.lustreAst.VarIdExpr(vName);
+            % First SOLUTION, Direct search O(n)
+            %             vars{end+1} = nasa_toLustre.lustreAst.LustreVar(...
+            %                 u_node{i},'real');
+            %             % defining u_node{i}
+            %             conds = cell(1,numel(table_elem)-1);
+            %             thens = cell(1,numel(table_elem));
+            %             for j=1:numel(table_elem)-1
+            %                 conds{j} = nasa_toLustre.lustreAst.BinaryExpr(...
+            %                     nasa_toLustre.lustreAst.BinaryExpr.EQ,...
+            %                     boundingi{i},nasa_toLustre.lustreAst.IntExpr(j));
+            %                 thens{j} = table_elem{j};
+            %             end
+            %             thens{numel(table_elem)} = table_elem{numel(table_elem)};
+            %             rhs = nasa_toLustre.lustreAst.IteExpr.nestedIteExpr(conds, thens);
+            %             body{end+1} = nasa_toLustre.lustreAst.LustreEq(u_node{i},rhs);
+            
+            
+            % Second solution: Binary search O(log(n))
+            [body, vars] = nasa_toLustre.lustreAst.IteExpr.binarySearch(...
+                table_elem, boundingi{i}, vName,...
+                'real', [], body, vars, vName);
         end
-
+        
     end
 end
 
