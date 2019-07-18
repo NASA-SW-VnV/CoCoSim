883120db6462baaae3ce57c660d7459e6185adc2
Francois <francois.conzelmann@nasa.gov>
2019-07-18 10:46:54 -0700

    fix multiplication computation

--- src/middleEnd/+nasa_toLustre/+blocks/+Stateflow/+utils/@MF2LusUtils/mtimesFun_To_Lustre.m
+++ src/middleEnd/+nasa_toLustre/+blocks/+Stateflow/+utils/@MF2LusUtils/mtimesFun_To_Lustre.m
@@ -6,6 +6,9 @@ function [code, dim] = mtimesFun_To_Lustre(x, x_dim, y, y_dim)
     % Author: Francois Conzelmann <francois.conzelmann@nasa.gov>
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
+    x_reshape = reshape(x, x_dim);
+    y_reshape = reshape(y, y_dim);
+    
     code={};
     multi = nasa_toLustre.lustreAst.BinaryExpr.MULTIPLY;
     plus = nasa_toLustre.lustreAst.BinaryExpr.PLUS;
@@ -20,25 +23,25 @@ function [code, dim] = mtimesFun_To_Lustre(x, x_dim, y, y_dim)
             x(z), y, false), 1:numel(x), 'UniformOutput', 0);
     elseif length(x_dim) <= 2 && length(y_dim) <= 2
         
-        
-        
         dim = [x_dim(1), y_dim(2)];
         
-        for i=1:x_dim(1)
-            for j=1:y_dim(2)
+        for j=1:x_dim(1)
+            for i=1:y_dim(2)
                 exp = {};
-                for k=1:y_dim(1)
-                    x_ind = mod((j+k-1) * x_dim(1) + i, numel(x));
-                    y_ind = mod((i-1) * x_dim(1) + j + k, numel(x));
-                    if (x_ind == 0)
-                        x_ind = numel(x);
+                for k=0:(x_dim(2)-1)
+                    x_ind = mod(i + k, x_dim(2));
+                    y_ind = mod(i + k, y_dim(1));
+                    if x_ind == 0
+                        x_ind = x_dim(2);
                     end
-                    if (y_ind == 0)
-                        y_ind = numel(x);
+                    if y_ind == 0
+                        y_ind = y_dim(1);
                     end
+                    ax_indice = [i, x_ind];
+                    ay_indice = [y_ind, j];
                     exp{end+1} = nasa_toLustre.lustreAst.BinaryExpr(multi, ...
-                        x(x_ind), ...
-                        y(y_ind),...
+                        x_reshape(i, x_ind), ...
+                        y_reshape(y_ind, j),...
                         false);
                 end
                 code{end+1} = nasa_toLustre.lustreAst.BinaryExpr.BinaryMultiArgs(plus, exp);
