f6d4f1083fd63af0e552a8b8aa21cad1097d27ff
Francois <francois.conzelmann@nasa.gov>
2019-08-01 17:36:49 -0700

    update Dockerfile and CoCoDocker

--- src/utils/CoCoDocker.m
+++ src/utils/CoCoDocker.m
@@ -1,6 +1,18 @@
 classdef CoCoDocker
-    %COCODOCKER manage docker containers.
-    
+    % COCODOCKER manage docker containers.
+    %% example using the API
+    % sharedFolder = '/path/to/cocosim_result/absolute'
+    % CoCoDocker.start(sharedFolder)
+    % CoCoDocker.exec(sharedFolder, 'lustrec -node absolute_PP -int="long long int" absolute_PP.lus')
+    % CoCoDocker.exec(sharedFolder, 'make -f absolute_PP.makefile')
+    % CoCoDocker.exec(sharedFolder, '/lus/absolute_PP.LUSTREC_absolute_PP < /lus/absolute_PP_input_values > absolute_PP_output_values')
+    % CocoDocker.stop()
+    %% in terminal (example)
+    % docker run -d --name=toto -v $(pwd):/lus cocotest
+    % docker exec toto lustrec -node absolute_PP -int="long long int" absolute_PP.lus
+    % docker exec toto make -f absolute_PP.makefile
+    % docker exec toto bash -c '/lus/absolute_PP.LUSTREC_absolute_PP < /lus/absolute_PP_input_values > absolute_PP_output_values'
+    % docker stop toto && docker rm toto
     properties
     end
     
@@ -29,7 +41,7 @@ classdef CoCoDocker
         
         function [c_name, errCode, stdout] = start(sharedFolder)
             c_name = CoCoDocker.getCurrentContainerName(true);
-            [errCode, stdout] = system(sprintf('docker run -d -name=%s -v %s:/lus cocosim/lustre', ...
+            [errCode, stdout] = system(sprintf('docker run -d --name=%s -v %s:/lus cocosim/lustre', ...
                 c_name, sharedFolder));
         end
         
@@ -53,7 +65,7 @@ classdef CoCoDocker
                     return;
                 end
             end
-            [errCode, stdout] = system(sprintf('docker exec %s %s', ...
+            [errCode, stdout] = system(sprintf('docker exec %s bash -c ''%s''', ...
                 c_name, cmd));
         end
         
